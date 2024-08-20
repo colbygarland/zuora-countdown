d3b34ac048b21313305ab657f05dc24d435cd5cb
Colby Garland <colbygarland@gmail.com>
2024-08-20 15:32:22 -0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
 import { Days } from '@/components/Days'
 
+const devConfidenceLevel = 60
+const lastUpdatedAt = 'August 20, 2024'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen flex-col place-items-center bg-gray-900 p-24 text-gray-200">
       <div className="grid gap-12 lg:grid-cols-2 lg:gap-2">
-        <div className="">
+        <div>
           <img className="max-h-[500px] w-9/12" src="/owly-wing-raised-speaking.svg" alt="" />
         </div>
-        <div className="">
+        <div>
           <h1 className="mb-12 text-5xl font-bold">How many days until Zuora launch?</h1>
           <Days />
+          <p className="text-5xl">
+            Dev Confidence level: <span className="font-mono">{devConfidenceLevel}%</span>
+          </p>
+          <p>Last updated at: {lastUpdatedAt}</p>
         </div>
       </div>
     </main>
--- components/Days.tsx
+++ components/Days.tsx
@@ -1,9 +1,19 @@
 'use client'
 
-export const Days = () => {
-  const launchDate = new Date('2024-12-02')
+const getDaysUntil = (date: string) => {
   const today = new Date()
-  const diffTime = launchDate.getTime() - today.getTime()
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-  return <h2 className="text-4xl text-white">{diffDays} days until Phase 1 launch</h2>
+  const d = new Date(date)
+  const diffTime = d.getTime() - today.getTime()
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+}
+
+export const Days = () => {
+  const launchDays = getDaysUntil('2024-12-02')
+  const devDoneDays = getDaysUntil('2024-10-31')
+  return (
+    <div className="mb-10">
+      <h2 className="mb-4 text-4xl text-white">{launchDays} days until Phase 1 launch</h2>
+      <h2 className="text-4xl text-white">{devDoneDays} days until Phase 1 dev complete</h2>
+    </div>
+  )
 }
